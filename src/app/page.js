7fdf882fd31cd7c49281ffba72ec5156b3e797f4
TNC9 <koonbenzkub@hotmail.com>
2023-07-25 18:40:26 +0700

    Finish

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,48 @@
 "use client";
 
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { UserCard } from "@/components/UserCard";
+import { cleanUser } from "@/libs/cleanUser";
 
 export default function RandomUserPage() {
   //user = null or array of object
   const [users, setUsers] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [genAmount, setGenAmount] = useState(1);
+  const [isFirstLoad, setIsFirstLoading] = useState(true);
+
+  useEffect(() => {
+    if (isFirstLoad) {
+      setIsFirstLoading(false);
+      return;
+    }
+    if (!isFirstLoad) {
+      const strTasks = JSON.stringify(genAmount);
+      localStorage.setItem("task", strTasks);
+    }
+  }, [genAmount]);
+
+  useEffect(() => {
+    const strTasks = JSON.parse(localStorage.getItem("task"));
+    if (strTasks == null) {
+      return;
+    }
+    setGenAmount(parseInt(strTasks));
+    // const loadedTasks = JSON.parse(genAmount);
+    // setGenAmount(loadedTasks);
+
+    const fetchData = async () => {
+      setIsLoading(true);
+      const resp = await axios.get(
+        `https://randomuser.me/api/?results=${strTasks}`
+      );
+      const users = resp.data.results.map(cleanUser);
+      setUsers(users);
+      setIsLoading(false);
+    };
+    fetchData();
+  }, []);
 
   const generateBtnOnClick = async () => {
     setIsLoading(true);
@@ -15,10 +50,11 @@ export default function RandomUserPage() {
       `https://randomuser.me/api/?results=${genAmount}`
     );
     setIsLoading(false);
-    const users = resp.data.results;
+    const users = resp.data.results.map(cleanUser);
     //Your code here
     //Process result from api response with map function. Tips use function from /src/libs/cleanUser
     //Then update state with function : setUsers(...)
+    setUsers(users);
   };
 
   return (
@@ -27,6 +63,7 @@ export default function RandomUserPage() {
       <div className="d-flex justify-content-center align-items-center fs-5 gap-2">
         Number of User(s)
         <input
+          min={1}
           className="form-control text-center"
           style={{ maxWidth: "100px" }}
           type="number"
@@ -40,7 +77,16 @@ export default function RandomUserPage() {
       {isLoading && (
         <p className="display-6 text-center fst-italic my-4">Loading ...</p>
       )}
-      {users && !isLoading && users.map(/*code map rendering UserCard here */)}
+      {users &&
+        !isLoading &&
+        users.map((user) => (
+          <UserCard
+            name={user.name}
+            imgUrl={user.imgUrl}
+            address={user.address}
+            email={user.email}
+          ></UserCard>
+        ))}
     </div>
   );
 }
